f8fdb3cd5a2952cf843f45c45c6ef901a2a091f1
Jerry Du <jduomaha@gmail.com>
2021-08-02 23:17:14 -0400

    Added TKinter

--- reddit-api.py
+++ reddit-api.py
@@ -1,6 +1,19 @@
 import praw
 from PIL import Image, ImageFilter
 import urllib.request
+from tkinter import *
+from tkinter.ttk import *
+from PIL import ImageTk, Image
+
+def user_gui():
+    root = Tk()
+    height = int(root.winfo_screenheight())
+    width = int(root.winfo_screenwidth())
+    canvas = Canvas(root, width = width, height = height)  
+    canvas.pack()  
+    img = ImageTk.PhotoImage(Image.open("yourname.jpg"))  
+    canvas.create_image(800, 450, anchor=CENTER, image=img) 
+    root.mainloop() 
 
 def login_to_reddit():
     reddit = praw.Reddit(
@@ -13,9 +26,8 @@ def login_to_reddit():
     print("Successfully logged into Reddit")
     return reddit
 
-
 def get_posts(reddit):
-    for submission in reddit.subreddit("spaceporn").hot(limit=None):
+        submission = reddit.subreddit("spaceporn").random()
         post_url = str(submission.url)
         one_post = False
         if post_url.endswith("jpg") or post_url.endswith("jpg") or post_url.endswith("png"):
@@ -24,10 +36,11 @@ def get_posts(reddit):
             img.show()
             print(post_url + " " + submission.title)
             one_post == True
-        break
+#Since the subreddit obviously has a limited number of posts, there is a chance where you get repeats
 def main():
     reddit = login_to_reddit()
     get_posts(reddit)
 
 if __name__ == "__main__":
     main()
+
